cae8b068763175a51e6abf99baa09848fd1caccc
anghelmihai2002 <Anghel.Ma.Mihai@student.utcluj.ro>
2024-01-14 20:53:50 +0200

    restore old project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Home from "./Home";
+import AboutPage from "./pages/AboutPage";
+import RoomsPage from "./pages/RoomsPage";
+import Competitions from "./pages/CompetitionsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/rooms" element={<RoomsPage />} />
+        <Route path="/competitions" element={<Competitions />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Navbar from "./components/navbar";
+import "./Home.css";
+
+export default function HomeExport() {
+  return (
+    <body>
+      <Navbar />
+    </body>
+  );
+}
